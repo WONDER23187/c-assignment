ac6e02e73ae8e68e395b8ce76e6fabb054906065
WONDER23187 <47216086+WONDER23187@users.noreply.github.com>
2019-12-11 00:09:24 +0800

    Update quiz4.cpp
    缺省构造，运算符==重载，传对象的引用作参数

--- quiz4/quiz4.cpp
+++ quiz4/quiz4.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 class File{
 public:
-	File(string = "", string = "");
-	bool operator==(const File&);
+	File(string = "", string = ""); //定义缺省构造
+	bool operator==(const File&); //运算符==重载
 private:
 	string name, type;
 };
-File::File(string _name, string _type)
+File::File(string _name, string _type) //定义
 {
 	name = _name;
 	type = _type;
 }
-bool File::operator==(const File& f)
+bool File::operator==(const File& f) //定义
 {
 	return type == f.type;
 }
 int main()
 {
-	File f1("aa", "txt"), f2("bb", "png");
+	File f1("aa", "txt"), f2("bb", "png"); //调用
 	if (f1 == f2)
 		cout << "the two file types are same\n";
 	else
